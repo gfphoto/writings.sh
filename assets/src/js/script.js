3021795119248ef8d8ed379b791f31a14736b301
hit9 <hit9@icloud.com>
2019-08-15 00:22:57 +0800

    Add ue-scroll back-to-top

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -2,6 +2,8 @@ function init () {
   // NProgress
   NProgress.start()
   window.onload = function () { NProgress.done() }
+  // UeScroll
+  UeScroll.init()
   // mediumZoom
   mediumZoom('article .post-body img')
 }

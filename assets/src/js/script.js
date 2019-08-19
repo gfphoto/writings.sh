fef34ec96e3daf9d4be90a3ad6cb267b66124814
hit9 <hit9@icloud.com>
2019-08-20 00:47:34 +0800

    Add background to mediumZoom

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -2,5 +2,7 @@ function init () {
   // UeScroll
   UeScroll.init()
   // mediumZoom
-  mediumZoom('article .post-body img')
+  mediumZoom('article .post-body img', {
+    background: 'rgba(153,153,153,0.6)'
+  })
 }

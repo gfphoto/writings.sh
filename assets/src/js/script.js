59760c8d5e9385eb6834d772ca5fea67fe8edfc4
hit9 <hit9@icloud.com>
2019-09-02 08:51:52 +0800

    Add lozad

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -1,4 +1,7 @@
 function init () {
+  // Lazyload
+  var observer = lozad() // lazy loads elements with default selector as '.lozad'
+  observer.observe()
   // UeScroll
   UeScroll.init()
   // mediumZoom

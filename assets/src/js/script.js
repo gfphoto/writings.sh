eb57c0b9d24634c05c21617c1594ded7cfbc6a04
hit9 <hit9@icloud.com>
2020-01-29 12:33:00 +0800

    Drop turbolinks

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -25,55 +25,70 @@ function init () {
   observer.observe({ threshold: 0.01 })
 
   // turbolinks
-  Turbolinks.setProgressBarDelay(200)
-  Turbolinks.start()
+  // Turbolinks.setProgressBarDelay(200)
+  // Turbolinks.start()
 
-  document.addEventListener('turbolinks:load', function (e) {
-    if (document.getElementsByClassName('post-body').length > 0) {
-      // var body = document.getElementsByTagName('body')[0]
-      // body.classList.add('fadein')
-      // Reload disqus count
-      if (typeof DISQUSWIDGETS === 'undefined') {
-        setTimeout(function () {
-          reloadDisqusCount()
-        }, 3000)
-      } else {
-        reloadDisqusCount()
-      }
-    }
-  }, false)
-  // https://stackoverflow.com/questions/20814531/local-mathjax-loading-with-turbolinks-in-rails-4
-  document.addEventListener('turbolinks:load', function (e) {
-    if (typeof window.MathJax === 'undefined') {
-      setTimeout(function () {
-        reconfigMath()
-      }, 2000)
-    } else {
+  // document.addEventListener('turbolinks:load', function (e) {
+  //   if (document.getElementsByClassName('post-body').length > 0) {
+  //     // var body = document.getElementsByTagName('body')[0]
+  //     // body.classList.add('fadein')
+  //     // Reload disqus count
+  //     if (typeof DISQUSWIDGETS === 'undefined') {
+  //       setTimeout(function () {
+  //         reloadDisqusCount()
+  //       }, 3000)
+  //     } else {
+  //       reloadDisqusCount()
+  //     }
+  //   }
+  // }, false)
+  // // https://stackoverflow.com/questions/20814531/local-mathjax-loading-with-turbolinks-in-rails-4
+  // document.addEventListener('turbolinks:load', function (e) {
+  //   if (typeof window.MathJax === 'undefined') {
+  //     setTimeout(function () {
+  //       reconfigMath()
+  //     }, 2000)
+  //   } else {
+  //     reconfigMath()
+  //   }
+  // }, false)
+  // // https://github.com/turbolinks/turbolinks/issues/75#issuecomment-443256173
+  // document.addEventListener('turbolinks:click', function (e) {
+  //   if (e.target.getAttribute('href').charAt(0) === '#') {
+  //     return e.preventDefault()
+  //   }
+  // }, false)
+  // document.addEventListener('turbolinks:load', function (e) {
+  //   if (window.location.hash) {
+  //     var hash = window.location.hash.substring(1)
+  //     var decodedHash = decodeURIComponent(hash)
+  //     var element = document.getElementById(decodedHash)
+  //     if (element != null) {
+  //       element.scrollIntoView()
+  //       element.classList.add('highlighted')
+  //     }
+  //   }
+  //
+  // // GA
+  // if (typeof ga !== 'undefined') {
+  //   ga('set', 'page', window.location.pathname)
+  //   ga('send', 'pageview')
+  // }
+  // }, false)
+
+  // GA
+  if (typeof ga !== 'undefined') {
+    ga('set', 'page', window.location.pathname)
+    ga('send', 'pageview')
+  }
+
+  if (typeof window.MathJax === 'undefined') {
+    setTimeout(function () {
       reconfigMath()
-    }
-  }, false)
-  // https://github.com/turbolinks/turbolinks/issues/75#issuecomment-443256173
-  document.addEventListener('turbolinks:click', function (e) {
-    if (e.target.getAttribute('href').charAt(0) === '#') {
-      return e.preventDefault()
-    }
-  }, false)
-  document.addEventListener('turbolinks:load', function (e) {
-    if (window.location.hash) {
-      var hash = window.location.hash.substring(1)
-      var decodedHash = decodeURIComponent(hash)
-      var element = document.getElementById(decodedHash)
-      if (element != null) {
-        element.scrollIntoView()
-        element.classList.add('highlighted')
-      }
-    }
-    // GA
-    if (typeof ga !== 'undefined') {
-      ga('set', 'page', window.location.pathname)
-      ga('send', 'pageview')
-    }
-  }, false)
+    }, 2000)
+  } else {
+    reconfigMath()
+  }
 
   // UeScroll
   UeScroll.init()

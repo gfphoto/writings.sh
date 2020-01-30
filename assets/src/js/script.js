6e427e449d88acb057c666642702650d479ec0ae
hit9 <hit9@icloud.com>
2020-01-30 13:31:44 +0800

    load all js in onload

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -19,57 +19,57 @@ function init () {
       ('\n') +
       ('欢迎光临！\n')
     )
-  }
-  // lozad
-  var observer = lozad('.lazyload')
-  observer.observe({ threshold: 0.01 })
+    // lozad
+    var observer = lozad('.lazyload')
+    observer.observe({ threshold: 0.01 })
 
-  // Disqus count
-  // if (document.getElementsByClassName('post-body').length > 0) {
-  //   // var body = document.getElementsByTagName('body')[0]
-  //   // body.classList.add('fadein')
-  //   // Reload disqus count
-  //   if (typeof DISQUSWIDGETS === 'undefined') {
-  //     setTimeout(function () {
-  //       reloadDisqusCount()
-  //     }, 3000)
-  //   } else {
-  //     reloadDisqusCount()
-  //   }
-  // }
+    // Disqus count
+    // if (document.getElementsByClassName('post-body').length > 0) {
+    //   // var body = document.getElementsByTagName('body')[0]
+    //   // body.classList.add('fadein')
+    //   // Reload disqus count
+    //   if (typeof DISQUSWIDGETS === 'undefined') {
+    //     setTimeout(function () {
+    //       reloadDisqusCount()
+    //     }, 3000)
+    //   } else {
+    //     reloadDisqusCount()
+    //   }
+    // }
 
-  // Location href
-  if (window.location.hash) {
-    var hash = window.location.hash.substring(1)
-    var decodedHash = decodeURIComponent(hash)
-    var element = document.getElementById(decodedHash)
-    if (element != null) {
-      element.scrollIntoView()
-      element.classList.add('highlighted')
+    // Location href
+    if (window.location.hash) {
+      var hash = window.location.hash.substring(1)
+      var decodedHash = decodeURIComponent(hash)
+      var element = document.getElementById(decodedHash)
+      if (element != null) {
+        element.scrollIntoView()
+        element.classList.add('highlighted')
+      }
     }
-  }
 
-  // MathJax
-  if (typeof window.MathJax === 'undefined') {
-    setTimeout(function () {
+    // MathJax
+    if (typeof window.MathJax === 'undefined') {
+      setTimeout(function () {
+        reconfigMath()
+      }, 2000)
+    } else {
       reconfigMath()
-    }, 2000)
-  } else {
-    reconfigMath()
+    }
+
+    // Gitalk (delay 2s)
+    setTimeout(function () { loadGitalk() }, 2000)
+
+    // GA
+    if (typeof ga !== 'undefined') {
+      ga('set', 'page', window.location.pathname)
+      ga('send', 'pageview')
+    }
+
+    // UeScroll
+    UeScroll.init()
+
+    // mediumZoom
+    mediumZoom('img.post-content-img')
   }
-
-  // Gitalk (delay 2s)
-  setTimeout(function () { loadGitalk() }, 2000)
-
-  // GA
-  if (typeof ga !== 'undefined') {
-    ga('set', 'page', window.location.pathname)
-    ga('send', 'pageview')
-  }
-
-  // UeScroll
-  UeScroll.init()
-
-  // mediumZoom
-  mediumZoom('img.post-content-img')
 }

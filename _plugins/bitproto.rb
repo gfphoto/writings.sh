7ef114e25aec26d04f24a19b04192f05804785e1
hit9 <hit9@icloud.com>
2021-02-05 16:57:29 +0800

    Update the lexer name

--- _plugins/bitproto.rb
+++ _plugins/bitproto.rb
@@ -2,7 +2,7 @@
 
 Jekyll::Hooks.register :site, :pre_render do |site|
   require "rouge"
-    class MoreJSLexer < Rouge::Lexers::Protobuf
+    class BitprotoLexer < Rouge::Lexers::Protobuf
       title 'bitproto'
       desc 'bitproto syntax lexer'
       tag 'bitproto'
